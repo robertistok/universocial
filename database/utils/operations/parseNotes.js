89038394b3dfb782c9ecc3f2bdbf6214a9ac337a
robertistok <robertistok94@gmail.com>
2017-01-30 09:17:17 +0200

    Grades
    Refactor the extraction function and extract grades into 2 separate
    files
    listedGrades for the mathematics
    summedGrades <- generate from here for all the other courses

--- database/utils/operations/parseNotes.js
+++ database/utils/operations/parseNotes.js
@@ -3,25 +3,35 @@ const csv = require('fast-csv');
 
 const stream = fs.createReadStream('../data/clean.csv');
 
-const notes = {};
+const notes = [];
+
+function sumGrades(data) {
+  if (data['disciplina obligatorie'].includes('matematica t1')) {
+    let grade = data['nota finala - disciplina obligatorie'];
+    grade = parseInt(grade, 10);
+    if (grade < 4 || isNaN(grade)) return;
+    if (!notes[grade]) {
+      notes[grade] = 1;
+    } else {
+      notes[grade] += 1;
+    }
+  }
+}
+
+function listGrades(data) {
+  if (data['disciplina obligatorie'].includes('matematica t1')) {
+    let grade = data['nota finala - disciplina obligatorie'];
+    grade = parseInt(grade, 10);
+    if (grade >= 4 && !isNaN(grade)) notes.push(grade);
+  }
+}
 
 csv
- .fromStream(stream, {
-   headers: true,
-   strictColumnHandling: true,
- })
- .on('data', (data) => {
-   if (data['disciplina obligatorie'].includes('matematica t1')) {
-     let grade = data['nota finala - disciplina obligatorie'];
-     grade = parseInt(grade, 10);
-     if (grade < 3 || isNaN(grade)) return;
-     if (!notes[grade]) {
-       notes[grade] = 1;
-     } else {
-       notes[grade] += 1;
-     }
-   }
- })
- .on('finish', () => {
-   fs.writeFile('../data/grades.json', JSON.stringify(notes, null, 4), (err) => { if (err) throw err; });
- });
+.fromStream(stream, {
+  headers: true,
+  strictColumnHandling: true,
+})
+.on('data', listGrades)
+.on('finish', () => {
+  fs.writeFile('../data/listedGrades.json', JSON.stringify(notes, null, 4), (err) => { if (err) throw err; });
+});
