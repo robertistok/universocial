5c3dc4132a589b3fe34663c94b17d8607848646e
robertistok <robertistok94@gmail.com>
2017-02-02 09:04:02 +0200

    GPA virtual for student
    By accesing the gpa property of the Student scheme, one can find out
    more about a student’s performance

--- database/models/student.js
+++ database/models/student.js
@@ -39,5 +39,47 @@ const StudentSchema = new Schema({
   }],
 });
 
+StudentSchema.virtual('gpa').
+  get(function() {
+    let totalCredits = 0;
+    return this.grades
+      .sort((a, b) => a.course.code - b.course.code)
+      .reduce((gpa, item) => {
+        console.log(totalCredits);
+        const year = `year${item.course.year}`;
+        const semester = `semester${item.course.semester}`;
+        const { credits } = item.course;
+        const { code } = item.course;
+        const { grade } = item;
+        totalCredits += credits;
+
+        if (!gpa[year]) {
+          gpa[year] = {};
+        }
+
+        if (!gpa[year][semester]) {
+          gpa[year][semester] = {
+            gpa: 0,
+            credits: 0,
+            examsLeft: [],
+          };
+        }
+
+        if (grade < 5) {
+          gpa[year][semester].examsLeft.push(code);
+        } else {
+          gpa[year][semester].credits += credits;
+          gpa[year][semester].gpa += grade * credits;
+        };
+
+        if (totalCredits === 30) {
+          gpa[year][semester].gpa /= 30;
+          totalCredits = 0;
+        }
+
+        return gpa;
+      }, {});
+  })
+
 const Student = mongoose.model('student', StudentSchema);
 module.exports = Student;
---
+++ database/queries/test.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Student = require('../models/student');
+const Grade = require('../models/grade');
+const Course = require('../models/course')
+
+mongoose.Promise = global.Promise;
+if (process.NODE_ENV !== 'test') {
+  mongoose
+    .connect('mongodb://localhost/universocial', {
+      server: {
+        socketOptions: {
+          socketTimeoutMS: 0,
+          connectTimeoutMS: 0
+        }
+      }
+    })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.log(err));
+}
+
+Student.findOne({ identificationNumber: 21021658 })
+  .populate({
+    path: 'grades',
+    populate: {
+      path: 'course',
+      model: 'course',
+    }
+  })
+  .then((student) => console.log(student.gpa))
